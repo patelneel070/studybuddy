540c206f658814a3103d8e779121941c1e6d8df9
patelneel070 <patelneel5481@gmail.com>
2026-02-09 10:07:38 -0800

    update

--- studybuddy/settings.py
+++ studybuddy/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -51,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
 ROOT_URLCONF = 'studybuddy.urls'
 
 TEMPLATES = [
